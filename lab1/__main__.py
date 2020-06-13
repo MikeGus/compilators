44188625d69d3dd88afbae4a3beba2e5cc05ddd6
MikeGus <vfc17@yandex.ru>
2020-06-13 20:00:05 +0300

    fixes

--- lab1/__main__.py
+++ lab1/__main__.py
@@ -7,14 +7,25 @@ def main():
         [True, True, True, False, False, True, True, False],
     ]
     for regexp, expected in zip(['ab', '(a|b)b*(c|d)*'], expected_responses):
-        print('Regexp: ', regexp)
+        # print('Regexp: ', regexp)
         dfa = DFA(regexp)
         dfa.minimize()
-        print('DFA:\n', dfa)
+        # print('DFA:\n', dfa)
         for s, e_r in zip(['a', 'ab', 'baaabbbbbbccdd', 'dddd', 'dddaa', 'bd', 'b', ''], expected):
             dfa_resp = dfa.simulate(s)
             assert(e_r == dfa_resp)
-            print('String: ', s, ' DFA response: ', dfa_resp, ' Expected: ', e_r)
+            # print('String: ', s, ' DFA response: ', dfa_resp, ' Expected: ', e_r)
+
+    regexp = '((ab)|(ab)*)e*f'
+    print(regexp)
+    dfa = DFA(regexp, set(list('abef')))
+    print(dfa)
+    dfa.minimize()
+    print(dfa)
+    s_list = ['abf', 'ab', 'abeef', 'abeee', 'abababf']
+    for s in s_list:
+        print('str: ', s)
+        print('res: ', dfa.simulate(s))
 
 
 if __name__ == '__main__':
